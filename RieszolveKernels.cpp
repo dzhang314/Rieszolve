e27f7563e7c7f1033d757f90eebc536a83bf4544
David K. Zhang <dzhang314@gmail.com>
2025-02-10 14:44:32 -0800

    restore AVX-512 kernels

--- RieszolveKernels.cpp
+++ RieszolveKernels.cpp
@@ -4,6 +4,16 @@
 using std::fma;
 using std::sqrt;
 
+#ifndef RIESZOLVE_DISABLE_AVX512
+#ifdef __AVX512F__
+#define RIESZOLVE_USE_AVX512
+#endif // __AVX512F__
+#endif // RIESZOLVE_DISABLE_AVX512
+
+#ifdef RIESZOLVE_USE_AVX512
+#include <immintrin.h>
+#endif // RIESZOLVE_USE_AVX512
+
 
 static inline void two_sum(double &s, double &e, double x, double y) noexcept {
     s = x + y;
@@ -15,11 +25,32 @@ static inline void two_sum(double &s, double &e, double x, double y) noexcept {
 }
 
 
+#ifdef RIESZOLVE_USE_AVX512
+static inline void two_sum(__m512d &s, __m512d &e, __m512d x, __m512d y) {
+    s = _mm512_add_pd(x, y);
+    const __m512d x_eff = _mm512_sub_pd(s, y);
+    const __m512d y_eff = _mm512_sub_pd(s, x_eff);
+    const __m512d x_err = _mm512_sub_pd(x, x_eff);
+    const __m512d y_err = _mm512_sub_pd(y, y_eff);
+    e = _mm512_add_pd(x_err, y_err);
+}
+#endif // RIESZOLVE_USE_AVX512
+
+
 static inline void two_sum(double &s, double &e) noexcept {
     const double x = s;
     const double y = e;
     two_sum(s, e, x, y);
 }
+
+
+#ifdef RIESZOLVE_USE_AVX512
+static inline void two_sum(__m512d &s, __m512d &e) {
+    const __m512d x = s;
+    const __m512d y = e;
+    two_sum(s, e, x, y);
+}
+#endif // RIESZOLVE_USE_AVX512
 
 
 struct HighPrecisionAccumulator {
@@ -52,12 +83,112 @@ struct HighPrecisionAccumulator {
 #endif // _OPENMP
 
 
+#ifdef RIESZOLVE_USE_AVX512
+
+struct HighPrecisionVectorAccumulator {
+
+    __m512d terms[2];
+
+    HighPrecisionVectorAccumulator() noexcept
+        : terms{_mm512_setzero_pd(), _mm512_setzero_pd()} {}
+
+    double to_double() const noexcept {
+        return _mm512_reduce_add_pd(_mm512_add_pd(terms[0], terms[1]));
+    }
+
+    void add(__m512d x) noexcept {
+        two_sum(terms[0], x);
+        two_sum(terms[1], x);
+    }
+
+    HighPrecisionVectorAccumulator &
+    operator+=(const HighPrecisionVectorAccumulator &other) noexcept {
+        add(other.terms[0]);
+        add(other.terms[1]);
+        return *this;
+    }
+
+}; // struct HighPrecisionVectorAccumulator
+
+#ifdef _OPENMP
+#pragma omp declare reduction(                                                 \
+        + : HighPrecisionVectorAccumulator : omp_out += omp_in                 \
+) initializer(omp_priv = HighPrecisionVectorAccumulator{})
+#endif // _OPENMP
+
+#endif // RIESZOLVE_USE_AVX512
+
+
 double compute_coulomb_energy(
     const double *__restrict__ points_x,
     const double *__restrict__ points_y,
     const double *__restrict__ points_z,
     int num_points
 ) noexcept {
+#ifdef RIESZOLVE_USE_AVX512
+    const __m512d ONE_VECTOR = _mm512_set1_pd(1.0);
+    HighPrecisionVectorAccumulator energy_vector;
+    HighPrecisionAccumulator energy_scalar;
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)                                      \
+    reduction(+ : energy_vector, energy_scalar)
+#endif // _OPENMP
+    for (int i = 0; i < num_points; ++i) {
+        const double xi = points_x[i];
+        const double yi = points_y[i];
+        const double zi = points_z[i];
+        const __m512d xi_vector = _mm512_set1_pd(xi);
+        const __m512d yi_vector = _mm512_set1_pd(yi);
+        const __m512d zi_vector = _mm512_set1_pd(zi);
+        for (int j = 0; j < num_points;) {
+            if (j + 8 <= num_points) {
+                if ((j <= i) & (i < j + 8)) {
+                    for (int k = 0; k < 8; ++j, ++k) {
+                        if (i != j) {
+                            const double delta_x = xi - points_x[j];
+                            const double delta_y = yi - points_y[j];
+                            const double delta_z = zi - points_z[j];
+                            const double dist_squared = delta_x * delta_x +
+                                                        delta_y * delta_y +
+                                                        delta_z * delta_z;
+                            energy_scalar.add(1.0 / sqrt(dist_squared));
+                        }
+                    }
+                } else {
+                    const __m512d delta_x =
+                        _mm512_sub_pd(xi_vector, _mm512_load_pd(points_x + j));
+                    const __m512d delta_y =
+                        _mm512_sub_pd(yi_vector, _mm512_load_pd(points_y + j));
+                    const __m512d delta_z =
+                        _mm512_sub_pd(zi_vector, _mm512_load_pd(points_z + j));
+                    const __m512d dist_squared = _mm512_add_pd(
+                        _mm512_add_pd(
+                            _mm512_mul_pd(delta_x, delta_x),
+                            _mm512_mul_pd(delta_y, delta_y)
+                        ),
+                        _mm512_mul_pd(delta_z, delta_z)
+                    );
+                    energy_vector.add(
+                        _mm512_div_pd(ONE_VECTOR, _mm512_sqrt_pd(dist_squared))
+                    );
+                    j += 8;
+                }
+            } else {
+                if (i != j) {
+                    const double delta_x = xi - points_x[j];
+                    const double delta_y = yi - points_y[j];
+                    const double delta_z = zi - points_z[j];
+                    const double dist_squared = delta_x * delta_x +
+                                                delta_y * delta_y +
+                                                delta_z * delta_z;
+                    energy_scalar.add(1.0 / sqrt(dist_squared));
+                }
+                ++j;
+            }
+        }
+    }
+    return energy_vector.to_double() + energy_scalar.to_double();
+#else
     HighPrecisionAccumulator energy;
 #ifdef _OPENMP
 #pragma omp parallel for schedule(static) reduction(+ : energy)
@@ -79,6 +210,7 @@ double compute_coulomb_energy(
         }
     }
     return energy.to_double();
+#endif // RIESZOLVE_USE_AVX512
 }
 
 
@@ -91,6 +223,95 @@ double compute_coulomb_forces(
     const double *__restrict__ points_z,
     int num_points
 ) noexcept {
+#ifdef RIESZOLVE_USE_AVX512
+    const __m512d ONE_VECTOR = _mm512_set1_pd(1.0);
+    HighPrecisionVectorAccumulator energy_vector;
+    HighPrecisionAccumulator energy_scalar;
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)                                      \
+    reduction(+ : energy_vector, energy_scalar)
+#endif // _OPENMP
+    for (int i = 0; i < num_points; ++i) {
+        const double xi = points_x[i];
+        const double yi = points_y[i];
+        const double zi = points_z[i];
+        const __m512d xi_vector = _mm512_set1_pd(xi);
+        const __m512d yi_vector = _mm512_set1_pd(yi);
+        const __m512d zi_vector = _mm512_set1_pd(zi);
+        HighPrecisionVectorAccumulator fx_vector;
+        HighPrecisionVectorAccumulator fy_vector;
+        HighPrecisionVectorAccumulator fz_vector;
+        HighPrecisionAccumulator fx_scalar;
+        HighPrecisionAccumulator fy_scalar;
+        HighPrecisionAccumulator fz_scalar;
+        for (int j = 0; j < num_points;) {
+            if (j + 8 <= num_points) {
+                if ((j <= i) & (i < j + 8)) {
+                    for (int k = 0; k < 8; ++j, ++k) {
+                        if (i != j) {
+                            const double delta_x = xi - points_x[j];
+                            const double delta_y = yi - points_y[j];
+                            const double delta_z = zi - points_z[j];
+                            const double dist_squared = delta_x * delta_x +
+                                                        delta_y * delta_y +
+                                                        delta_z * delta_z;
+                            const double inv_dist = 1.0 / sqrt(dist_squared);
+                            energy_scalar.add(inv_dist);
+                            const double inv_dist_cubed =
+                                inv_dist / dist_squared;
+                            fx_scalar.add(delta_x * inv_dist_cubed);
+                            fy_scalar.add(delta_y * inv_dist_cubed);
+                            fz_scalar.add(delta_z * inv_dist_cubed);
+                        }
+                    }
+                } else {
+                    const __m512d delta_x =
+                        _mm512_sub_pd(xi_vector, _mm512_load_pd(points_x + j));
+                    const __m512d delta_y =
+                        _mm512_sub_pd(yi_vector, _mm512_load_pd(points_y + j));
+                    const __m512d delta_z =
+                        _mm512_sub_pd(zi_vector, _mm512_load_pd(points_z + j));
+                    const __m512d dist_squared = _mm512_add_pd(
+                        _mm512_add_pd(
+                            _mm512_mul_pd(delta_x, delta_x),
+                            _mm512_mul_pd(delta_y, delta_y)
+                        ),
+                        _mm512_mul_pd(delta_z, delta_z)
+                    );
+                    const __m512d inv_dist =
+                        _mm512_div_pd(ONE_VECTOR, _mm512_sqrt_pd(dist_squared));
+                    energy_vector.add(inv_dist);
+                    const __m512d inv_dist_cubed =
+                        _mm512_div_pd(inv_dist, dist_squared);
+                    fx_vector.add(_mm512_mul_pd(delta_x, inv_dist_cubed));
+                    fy_vector.add(_mm512_mul_pd(delta_y, inv_dist_cubed));
+                    fz_vector.add(_mm512_mul_pd(delta_z, inv_dist_cubed));
+                    j += 8;
+                }
+            } else {
+                if (i != j) {
+                    const double delta_x = xi - points_x[j];
+                    const double delta_y = yi - points_y[j];
+                    const double delta_z = zi - points_z[j];
+                    const double dist_squared = delta_x * delta_x +
+                                                delta_y * delta_y +
+                                                delta_z * delta_z;
+                    const double inv_dist = 1.0 / sqrt(dist_squared);
+                    energy_scalar.add(inv_dist);
+                    const double inv_dist_cubed = inv_dist / dist_squared;
+                    fx_scalar.add(delta_x * inv_dist_cubed);
+                    fy_scalar.add(delta_y * inv_dist_cubed);
+                    fz_scalar.add(delta_z * inv_dist_cubed);
+                }
+                ++j;
+            }
+        }
+        forces_x[i] = fx_vector.to_double() + fx_scalar.to_double();
+        forces_y[i] = fy_vector.to_double() + fy_scalar.to_double();
+        forces_z[i] = fz_vector.to_double() + fz_scalar.to_double();
+    }
+    return energy_vector.to_double() + energy_scalar.to_double();
+#else
     HighPrecisionAccumulator energy;
 #ifdef _OPENMP
 #pragma omp parallel for schedule(static) reduction(+ : energy)
@@ -122,6 +343,7 @@ double compute_coulomb_forces(
         forces_z[i] = fz.to_double();
     }
     return energy.to_double();
+#endif // RIESZOLVE_USE_AVX512
 }
 
 
