ca429a88d7228571610194ef3a8c69bba1270d82
David K. Zhang <dzhang314@gmail.com>
2025-06-01 10:42:17 -0700

    add vertices, edge centers, and face centers

--- scripts/SphericalSymmetries.jl
+++ scripts/SphericalSymmetries.jl
@@ -5,7 +5,8 @@ using StaticArrays: SA
 ################################################################################
 
 
-export chiral_icosahedral_group, full_icosahedral_group
+export chiral_icosahedral_group, full_icosahedral_group,
+    icosahedron_vertices, icosahedron_edge_centers, icosahedron_face_centers
 
 
 @inline function chiral_icosahedral_group(::Type{T}) where {T}
@@ -214,6 +215,115 @@ end
         -hpsi -half -hphi; +half +hphi +hpsi; -hphi -hpsi -half;;;
         -hpsi -half -hphi; -half -hphi -hpsi; +hphi +hpsi +half;;;
         -hpsi -half -hphi; -half -hphi -hpsi; -hphi -hpsi -half
+    ]
+end
+
+
+@inline function icosahedron_vertices(::Type{T}) where {T}
+    _zero = zero(T)
+    _one = one(T)
+    _two = _one + _one
+    _four = _two + _two
+    _five = _four + _one
+    _ten = _five + _five
+    _sqrt_five = sqrt(_five)
+    a = sqrt((_five + _sqrt_five) / _ten)
+    b = sqrt((_five - _sqrt_five) / _ten)
+    return SA{T}[
+        _zero; +a; +b;;
+        _zero; +a; -b;;
+        _zero; -a; +b;;
+        _zero; -a; -b;;
+        +b; _zero; +a;;
+        +b; _zero; -a;;
+        -b; _zero; +a;;
+        -b; _zero; -a;;
+        +a; +b; _zero;;
+        +a; -b; _zero;;
+        -a; +b; _zero;;
+        -a; -b; _zero
+    ]
+end
+
+
+@inline function icosahedron_edge_centers(::Type{T}) where {T}
+    _zero = zero(T)
+    _one = one(T)
+    _two = _one + _one
+    _four = _two + _two
+    _five = _four + _one
+    _sqrt_five = sqrt(_five)
+    half = inv(_two)
+    hphi = (_one + _sqrt_five) / _four # half of the golden ratio
+    hpsi = (_one - _sqrt_five) / _four # half of the golden ratio conjugate
+    return SA{T}[
+        +_one; _zero; _zero;;
+        -_one; _zero; _zero;;
+        _zero; +_one; _zero;;
+        _zero; -_one; _zero;;
+        _zero; _zero; +_one;;
+        _zero; _zero; -_one;;
+        +half; +hphi; +hpsi;;
+        +half; +hphi; -hpsi;;
+        +half; -hphi; +hpsi;;
+        +half; -hphi; -hpsi;;
+        -half; +hphi; +hpsi;;
+        -half; +hphi; -hpsi;;
+        -half; -hphi; +hpsi;;
+        -half; -hphi; -hpsi;;
+        +hphi; +hpsi; +half;;
+        +hphi; +hpsi; -half;;
+        +hphi; -hpsi; +half;;
+        +hphi; -hpsi; -half;;
+        -hphi; +hpsi; +half;;
+        -hphi; +hpsi; -half;;
+        -hphi; -hpsi; +half;;
+        -hphi; -hpsi; -half;;
+        +hpsi; +half; +hphi;;
+        +hpsi; +half; -hphi;;
+        +hpsi; -half; +hphi;;
+        +hpsi; -half; -hphi;;
+        -hpsi; +half; +hphi;;
+        -hpsi; +half; -hphi;;
+        -hpsi; -half; +hphi;;
+        -hpsi; -half; -hphi
+    ]
+end
+
+
+@inline function icosahedron_face_centers(::Type{T}) where {T}
+    _zero = zero(T)
+    _one = one(T)
+    _two = _one + _one
+    _three = _two + _one
+    _four = _two + _two
+    _five = _four + _one
+    _six = _four + _two
+    _sqrt_five = sqrt(_five)
+    t = inv(sqrt(_three))
+    c = sqrt((_three + _sqrt_five) / _six)
+    d = sqrt((_three - _sqrt_five) / _six)
+    return SA{T}[
+        +t; +t; +t;;
+        +t; +t; -t;;
+        +t; -t; +t;;
+        +t; -t; -t;;
+        -t; +t; +t;;
+        -t; +t; -t;;
+        -t; -t; +t;;
+        -t; -t; -t;;
+        _zero; +d; +c;;
+        _zero; +d; -c;;
+        _zero; -d; +c;;
+        _zero; -d; -c;;
+        +c; _zero; +d;;
+        +c; _zero; -d;;
+        -c; _zero; +d;;
+        -c; _zero; -d;;
+        +d; +c; _zero;;
+        +d; -c; _zero;;
+        -d; +c; _zero;;
+        -d; -c; _zero
     ]
 end
 
